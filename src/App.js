acec95cbb1c84dd56ab6c537f9482264c3192d0d
Sasuke1503 <hoangtv.spk@gmail.com>
2022-08-29 11:11:03 +0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+import {useEffect, useState} from "react"
+import Content from './Content';
+
+function App() {
+  const [show, setShow] = useState(false)
+  return (
+    
+    <div >
+    <button onClick={()=> setShow(!show)}>Toggle</button>
+        {show && <Content/>}
+    </div>
+  );
+}
+
+export default App;
